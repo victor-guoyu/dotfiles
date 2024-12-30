1685584ba9e4853cd8c2f11c00218084346132e9
Victor Guo <victor.guoyu@hotmail.com>
2024-12-30 22:30:02 +0000

    update installation script

--- install.sh
+++ install.sh
@@ -3,21 +3,28 @@
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 
 # symlink dotfiles
-configs=(
-  "hammerspoon"
-  "alacritty"
-)
-for config in ${configs[@]}; do
-  dest=${HOME}/.${config}
-  if [ -L ${dest} ]; then
-    echo "${dest} symlink already exists, skipping"
-  elif [ -f ${dest} ]; then
-    echo "${dest} is a file, skipping"
-  elif [ -d ${dest} ]; then
-    echo "${dest} is a dir, skipping"
-  else
-    ln -s ~/dotfiles/${config} ${dest}
-  fi
-done
 
-ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+link_dotfiles() {
+  local src="$1"
+  local destination="$2"
+
+  if [ -L "${destination}" ]; then
+    echo "${destination} symlink already exists, skipping"
+  elif [ -f ${destination} ]; then
+    echo "${destination} is a file, skipping"
+  elif [ -d ${destination} ]; then
+    echo "${destination} is a dir, skipping"
+  else
+    echo "symlinking ${src}"
+    ln -s ~/dotfiles/"${src}" "${destination}"
+  fi
+}
+
+# alacritty
+link_dotfiles "alacritty" "${XDG_CONFIG_HOME}/alacritty"
+
+# hammerspoon
+link_dotfiles "hammerspoon" "${HOME}/.hammerspoon"
+
+# tmux
+link_dotfiles "tmux/tmux.conf" "${HOME}/.tmux.config"
