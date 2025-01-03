a787bc5b5a675ebb1d6e81109a947dae792a7e5f
Victor Guo <victor.guoyu@hotmail.com>
2025-01-03 02:35:32 +0000

    toggle apps

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,10 +1,30 @@
 hs.application.enableSpotlightForNameSearches(true)
 
-hs.hotkey.bind({ "cmd", "alt" }, "`", function()
-	local alacritty = hs.application.find("Alacritty")
-	if alacritty ~= nil and alacritty:isFrontmost() then
-		alacritty:hide()
-	else
-		hs.application.launchOrFocus("/Applications/Alacritty.app")
-	end
-end)
+local function toggleApp(settings)
+	hs.hotkey.bind(settings.mods, settings.key, function()
+		local app = hs.application.find(settings.app)
+		if app ~= nil and app:isFrontmost() then
+			app:hide()
+		else
+			hs.application.launchOrFocus("/Applications/" .. settings.app .. ".app")
+		end
+	end)
+end
+
+toggleApp({
+	app = "Alacritty",
+	mods = { "cmd", "alt" },
+	key = "`",
+})
+
+toggleApp({
+	app = "Slack",
+	mods = { "cmd", "shift" },
+	key = "s",
+})
+
+toggleApp({
+	app = "Chrome",
+	mods = { "cmd", "alt" },
+	key = "1",
+})
