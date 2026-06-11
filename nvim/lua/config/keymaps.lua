fe225e30f577b01400a62b94156c485a41568629
Victor Guo <victor.guoyu@hotmail.com>
2026-06-11 14:21:01 -0400

    make arrow keys work

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Option+Arrow word-wise motion. Ghostty sends <M-b>/<M-f> on macOS for
+-- Option+Left/Right, so both forms are mapped to keep parity with native macOS apps.
+vim.keymap.set({ "i", "n" }, "<M-Left>", "<C-Left>")
+vim.keymap.set({ "i", "n" }, "<M-Right>", "<C-Right>")
+vim.keymap.set({ "i", "n" }, "<M-b>", "<C-Left>")
+vim.keymap.set({ "i", "n" }, "<M-f>", "<C-Right>")
+
+-- Option+Delete deletes the previous word (macOS convention).
+vim.keymap.set("i", "<M-BS>", "<C-w>")
